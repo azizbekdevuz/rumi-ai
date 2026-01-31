aaaaa5c48f84636808ef40ad499bc2fa0717952a
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2026-02-01 01:38:23 +0900

    chore: remove unused files and exports

--- src/components/motion/index.ts
+++
@@ -1,7 +0,0 @@
-// Motion component exports
-export { Section, Container } from './Section';
-export { ParallaxLayer, ParallaxContainer, ScrollProgressBar } from './ParallaxLayer';
-
-// Re-export types
-export type { SectionProps, ContainerProps } from './Section';
-export type { ParallaxLayerProps, ParallaxContainerProps, ScrollProgressBarProps } from './ParallaxLayer';
