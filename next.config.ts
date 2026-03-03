c032c73dad1373904c691981ee79d872e948fff9
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2026-03-03 22:10:30 +0900

    feat(frontend): add Kakao CDN domains to Next.js image config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'k.kakaocdn.net' },
+      { protocol: 'http', hostname: 'k.kakaocdn.net' },
+      { protocol: 'https', hostname: 't1.kakaocdn.net' },
+      { protocol: 'http', hostname: 't1.kakaocdn.net' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'http', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
 };
 
 export default nextConfig;
